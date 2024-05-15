796097eca679b530e5353da71e2616b1006e6589
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-05-15 18:03:51 +0600

    filter sort removed search added

--- index.js
+++ index.js
@@ -54,23 +54,23 @@ async function run() {
         app.get('/filteredServices', async (req, res) => {
             const size= parseInt(req.query.size);
             const page=parseInt(req.query.page)-1;
-            const filter= req.query.filter
             console.log(size,page)
-
-
+            const search= req.query.search
+            
             let query={};
-
-            if(filter) query= {servicename: filter}
+            
+            if (search) {
+                query.servicename = { $regex: search, $options: 'i' };
+            }
+            
+            
 
             const result = await serviceCollection.find(query).skip(size*page).limit(size).toArray()
             res.send(result);
         })
 
         app.get('/services-count',async(req,res)=>{
-            const filter=req.query.filter;
-            let query={};
-            if(filter) query={servicename: filter}
-            const count=await serviceCollection.countDocuments(query);
+            const count=await serviceCollection.countDocuments();
             res.send({count})
         })
 
